bd13ce525f0b3717fc4cebd9f82317596c52f904
Salome Treichler <salome.treichler@noseryoung.ch>
2020-10-20 14:48:28 +0200

    Added RegisterPage.js

--- src/components/pages/RegisterPage.js
+++ src/components/pages/RegisterPage.js
@@ -7,53 +7,54 @@ import TextField from '@material-ui/core/TextField';
 import CustomButton from "../atoms/CustomButton";
 
 const useStyles = makeStyles((theme) => ({
-    loginCard: {
-        height: "375px",
+    registerCard: {
+        height: "390px",
         width: "500px",
         backgroundColor: theme.palette.common.lightgray,
-        padding: "50px 100px 0px 100px"
+        padding: "40px 100px 0px 100px"
     },
     container: {
         height: "100%",
     },
-    link:{
+    link: {
         color: theme.palette.secondary.main,
     },
     textField: {
         width: "100%",
     },
-    registerLink: {
-        fontSize: "12px",
-        marginTop: "12px"
-    },
-    item:{
+    item: {
         width: "100%"
     },
 }));
 
-export default function LoginPage() {
+export default function RegisterPage() {
     const classes = useStyles();
 
     return (
         <Grid container alignItems={"center"} justify={"center"} direction={"row"} className={classes.container}>
             <Grid item>
-                <Paper square className={classes.loginCard} elevation={2}>
-                    <Grid container alignItems={"center"} direction={"column"} className={classes.container} spacing={3}>
+                <Paper square className={classes.registerCard} elevation={2}>
+                    <Grid container alignItems={"center"} direction={"column"} className={classes.container}
+                          spacing={3}>
                         <Grid item>
-                            <Typography variant={"h4"}>Login</Typography>
+                            <Typography variant={"h4"}>Register</Typography>
                         </Grid>
                         <Grid item className={classes.item}>
                             <form className={classes.root} noValidate autoComplete="off">
-                                <TextField className={classes.textField} id="username" label="Username" required color={"secondary"}/>
+                                <TextField className={classes.textField} id="username" label="Username" required
+                                           color={"secondary"}/>
                                 <br/>
                                 <br/>
-                                <TextField className={classes.textField} id="password" label="Password" required color={"secondary"}/>
+                                <TextField className={classes.textField} id="password" label="Password" required
+                                           color={"secondary"}/>
                                 <br/>
-                                <Typography className={classes.registerLink}>Don’t have an account yet? Register <a href={"register"} className={classes.link}>here</a></Typography>
+                                <br/>
+                                <TextField className={classes.textField} id="password" label="Confirm Password" required
+                                           color={"secondary"}/>
                             </form>
                         </Grid>
                         <Grid item>
-                            <CustomButton text={"Login"}/>
+                            <CustomButton text={"Register"}/>
                         </Grid>
                     </Grid>
                 </Paper>
